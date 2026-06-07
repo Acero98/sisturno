84f68d61ac2eeb8c2b4bd56f9277103c270619b1
Acero98 <aceroolivaroman@gmail.com>
2026-06-07 13:14:46 -0500

    busqueda usuarios
    busqueda usuario

--- public/js/socket_config.js
+++ public/js/socket_config.js
@@ -1,2 +1,2 @@
-const SOCKET_URL = "http://localhost:3000";
-//const SOCKET_URL = "http://192.168.0.6:3000";
+//const SOCKET_URL = "http://localhost:3000";
+const SOCKET_URL = "http://192.168.0.6:3000";
