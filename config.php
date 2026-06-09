a469488ed41bb32bad4110032747214c2757c52a
Acero98 <101023068+Acero98@users.noreply.github.com>
2026-06-09 18:59:20 -0500

    camb
    camb

--- config.php
+++ config.php
@@ -1,5 +1,5 @@
 <?php
-
+/*
 define("BASE_URL", "http://localhost/sisturno/");
 define("SOCKETURL", "http://localhost:3000");
 define("BASE_FAVICON", "http://localhost/sisturno/assets/img/epsilo-favicon.png");
@@ -7,15 +7,15 @@ define("BASE_LOGO_LOGIN", "http://localhost/sisturno/assets/img/epsilo-logo.png"
 define("BASE_LOGO_BLANCO", "http://localhost/sisturno/assets/img/epsilo40x47.png");
 
 define("URL_VIDEO", "http://localhost/sisturnomedios/videos/");
-define("URL_AUDIO", "http://localhost/sisturnomedios/audios/");
+define("URL_AUDIO", "http://localhost/sisturnomedios/audios/");*/
 
-/*
-define("BASE_URL", "http://192.168.0.6/sisturno/");
-define("SOCKETURL", "http://192.168.0.6:3000");
-define("BASE_FAVICON", "http://192.168.0.6/sisturno/assets/img/epsilo-favicon.png");
-define("BASE_LOGO_LOGIN", "http://192.168.0.6/sisturno/assets/img/epsilo-logo.png");
-define("BASE_LOGO_BLANCO", "http://192.168.0.6/sisturno/assets/img/epsilo40x47.png");
 
-define("URL_VIDEO", "http://192.168.0.6/sisturnomedios/videos/");
-define("URL_AUDIO", "http://192.168.0.6/sisturnomedios/audios/");*/
+define("BASE_URL", "http://192.168.0.35/sisturno/");
+define("SOCKETURL", "http://192.168.0.35:3000");
+define("BASE_FAVICON", "http://192.168.0.35/sisturno/assets/img/epsilo-favicon.png");
+define("BASE_LOGO_LOGIN", "http://192.168.0.35/sisturno/assets/img/epsilo-logo.png");
+define("BASE_LOGO_BLANCO", "http://192.168.0.35/sisturno/assets/img/epsilo40x47.png");
+
+define("URL_VIDEO", "http://192.168.0.35/sisturnomedios/videos/");
+define("URL_AUDIO", "http://192.168.0.35/sisturnomedios/audios/");
 ?>
